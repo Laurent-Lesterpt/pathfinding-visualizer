f2dd1c3db1cfa9bd167639df9326cadd8a704978
Laurent Lesterpt <laurentlesterpt@protonmail.com>
2019-10-21 00:21:42 +0200

    Add bogus dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import PathfindingVisualizer from './PathfindingVisualizer/PahtfindingVisualizer'
+import Dropdown from './DropdownMenu/DropdownMenu'
 
 function App() {
   return (
     <div className="App">
+      <Dropdown></Dropdown>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   )
