900bf435c30c0a4f3940a1a8d79832c9d46910d2
Laurent Lesterpt <laurentlesterpt@protonmail.com>
2019-11-07 19:15:51 +0100

    Modify single test to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux'
+import gridReducer from './store/gridReducer'
+import tutorialReducer from './store/turorialReducer'
+
+const rootReducer = combineReducers({
+  gridReducer: gridReducer,
+  tutorialReducer: tutorialReducer
+})
+const store = createStore(rootReducer)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
